81fa6f8e2350abf639fdf76fd960e4652e224996
petervanya <peter.vanya@gmail.com>
2016-08-16 17:54:59 +0100

    New folder

---
+++ WaterMEOH_code/Old/default_mw_input.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Temporary production of input.yaml file
+# 28/07/16
+
+fname="input.yaml"
+
+cat >$fname <<EOF
+# Methods:
+# 1. aii = 25 kT, da ~chi from FH
+# 2. aii based on compressibility, da = sqrt(a11 a22) + ~chi
+# 3. from Travis, JCP, 2007
+
+dpd-density: 3.0
+box-size: 10.0            # in nm
+temperature: 300.0        # in K
+dt: 0.05
+run-time: 10              # in ns
+#water-meoh-ratio: 1.0     # number ratio of water to meoh molecules
+water-molar-fraction: 0.5 # fraction of water molecules
+method: 1
+waters-in-bead: 2
+meohs-in-bead: 1
+EOF
+echo "Parameter file saved in $fname"
