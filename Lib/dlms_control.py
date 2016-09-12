8b275aead9d40065faf9c4293c06bf5fdc57b6c8
petervanya <peter.vanya@gmail.com>
2016-09-12 14:56:17 +0100

    Adding functions

--- Lib/dlms_control.py
+++ Lib/dlms_control.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 """Usage:
-    dlms_control.py [--L <L> --dt <dt> --steps <n> --thermo <th> --halo <h>]
+    dlms_control.py [--L <L> --dt <dt> --steps <n> --startstep <st>]
+                    [--thermo <th> --halo <h>]
 
 Generate DL_MESO control file.
 
 Options:
      --L <L>            Box length [default: 40.0]
      --dt <dt>          Timestep [default: 0.05]
-     --steps <n>        Number of steps [default: 10000]
+     --steps <n>        Number of simulation steps [default: 10000]
+     --startstep <st>   Start timestep for dumping frames [default: 0]
      --thermo <th>      Print every [default: 100]
      --halo <h>         Boundary halo, like neighbor [default: 2.5]
 
@@ -21,25 +23,26 @@ args = docopt(__doc__)
 L = float(args["--L"])
 dt = float(args["--dt"])
 N = int(args["--steps"])
+startstep = int(args["--startstep"])
 thermo = int(args["--thermo"])
 halo = float(args["--halo"])
 
 
 s = "pokus\n\n"
 
-s += "volume " + str(L**3) + "\n"
+s += "volume %.2f\n" % L**3
 s += "temperature 1.0\n"
 s += "cutoff 1.0\n"
-s += "boundary halo " + str(halo) + "\n\n"
+s += "boundary halo %.1f\n\n" % halo
 
-s += "timestep " + str(dt) + "\n"
-s += "steps " + str(N) + "\n"
+s += "timestep %.2f\n" % dt
+s += "steps %i\n" % N
 s += "equilibration steps 0\n"
 s += "scale temperature every 10\n"
-s += "trajectory 0 100\n"
+s += "trajectory %i 100\n" % startstep
 s += "stats every 100\n"
 s += "stack size 100\n"
-s += "print every " + str(thermo) + "\n\n"
+s += "print every %i\n\n" % thermo
 
 s += "job time 1000000.0\n"
 s += "close time 1.0\n\n"
@@ -50,4 +53,5 @@ s += "finish\n"
 print("Box size: %.1f | Timestep: %.3f | Num steps: %i" % (L, dt, N))
 open("CONTROL", "w").write(s)
 print("CONTROL file saved.")
+
 
