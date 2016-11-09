2ac1373ecdf0ee8b0211e20af3aa0e1584c03da8
petervanya <peter.vanya@gmail.com>
2016-11-09 18:03:13 +0000

    Denoted electrodes

--- PTFE_code/plot_ptfe_profile.py
+++ PTFE_code/plot_ptfe_profile.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 """Usage:
-    plot_ptfe_profile.py <fileW> <fileB> [--boxsize <L> --pdf]
+    plot_ptfe_profile.py <fileW> <fileB> [--boxsize <L> --slab <sl> --pdf]
 
-[AD HOC] script to format plotting of normalised density profiles
-for water and PTFE backbone. (Previously also plotted sulfonic acid groups.)
+[AD HOC] Plot normalised 1d density profiles of water and PTFE backbone.
+Can add lines denoting slab width.
 
 Options:
-    --boxsize <L>       Box size [default: 20]
+    --boxsize <L>       Box size in DPD units [default: 40]
+    --slab <sl>         Slab width in nm
     --pdf               Plot as pdf
 
 25/02/16
@@ -17,35 +18,37 @@ import matplotlib
 import sys, os
 from docopt import docopt
 
-rc = 8.14e-10
+r_DPD = 8.14e-10
 matplotlib.rcParams.update({'font.size': 16})
 
 args = docopt(__doc__)
-fileW = args["<fileW>"]
-fileB = args["<fileB>"]
+fileW, fileB = args["<fileW>"], args["<fileB>"]
 if not os.path.isfile(fileW) or not os.path.isfile(fileB):
-    print("One or two files not found.")
-    sys.exit()
+    sys.exit("One or two files not found.")
+
+print("Plotting: %s | %s" % (fileW, fileB))
     
-L = float(args["--boxsize"])
-lw = 3.0
+L = float(args["--boxsize"]) * r_DPD * 1e9
+
+if args["--slab"]:
+    D = float(args["--slab"])
+    plt.axvspan(0.0, (L - D) / 2, facecolor='black', alpha=0.1)
+    plt.axvspan((L + D) / 2, L, facecolor='black', alpha=0.1)
 
 A = np.loadtxt(fileB)
-A[:, 0] *= rc*1e9   # convert to nm
+A[:, 0] *= r_DPD * 1e9   # convert to nm
 A[:, 1] /= sum(A[:, 1])
-plt.plot(A[:, 0], A[:, 1], "red", label="backbone", linewidth=lw)
+plt.plot(A[:, 0], A[:, 1], "red", label="backbone", linewidth=2)
 
 A = np.loadtxt(fileW)
-A[:, 0] *= rc*1e9
+A[:, 0] *= r_DPD * 1e9
 A[:, 1] /= sum(A[:, 1])
-plt.plot(A[:, 0], A[:, 1], "blue", label="water", linewidth=lw)
+plt.plot(A[:, 0], A[:, 1], "blue", label="water", linewidth=4)
 
 plt.xlabel("$x$ (nm)", fontsize=20)
 plt.xlim([0.5, np.max(A[:, 0]) - 0.5])
-plt.legend(loc="best")
 
-imgname = "profile_1d.png"
-if args["--pdf"]: imgname = "profile_1d.pdf"
+imgname = "profile_1d.pdf" if args["--pdf"] else "profile_1d.png"
 plt.savefig(imgname)
 print("Plot saved in", imgname)
 
