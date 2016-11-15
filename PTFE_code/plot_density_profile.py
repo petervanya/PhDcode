cf989b51c17c7d080ccca499ff2a0232e4c5aafd
petervanya <peter.vanya@gmail.com>
2016-11-15 14:32:19 +0000

    Improved plotting

--- PTFE_code/plot_density_profile.py
+++ PTFE_code/plot_density_profile.py
@@ -24,9 +24,12 @@ cm = args["--cmap"]
 print("Plotting density profile, colormap: %s" % cm)
 plt.imshow(A, cmap=cm, vmin=0.0, vmax=3.0)
 #plt.colorbar()
-plt.axis("off")
-outname = fname.rstrip("out") + "png"
-plt.savefig(outname)
-print("2d density profile plot saved in %s." % outname)
+#plt.axis("off")
+ax = plt.axes()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+figname = fname.rstrip("out") + "png"
+plt.savefig(figname, bbox_inches="tight")
+print("2d density profile plot saved in %s." % figname)
 
 
