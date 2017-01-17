2a4eb8e54cf59da4d0e43fc4a3a3a5cc7e715dee
petervanya <peter.vanya@gmail.com>
2017-01-17 20:31:43 +0000

    Polishing

--- PTFE_code/phase_plot2.py
+++ PTFE_code/phase_plot2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """Usage:
-    phase_plot.py <fB> <fS> <fW> <fE> [--L <L> --slab <d> --pdf]
+    phase_plot2.py <fB> <fS> <fW> <fE> [--L <L> --slab <d> --pdf --out <out>]
 
 Options:
     --L <L>        Box size in DPD units [default: 40]
     --slab <d>     Slab width in nm [default: 10]
+    --out <out>    Output name, no extension [default: phase_plot]
 
 [AD HOC] Make a figure plot as in Dura presentation.
 
@@ -17,12 +18,15 @@ import glob, sys
 from docopt import docopt
 
 rc = 8.14e-10
-matplotlib.rcParams.update({'font.size': 16})
+matplotlib.rcParams.update({'font.size': 24})
+
+
+args = docopt(__doc__)
 if args["--pdf"]:           # use latex fonts
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
 
-args = docopt(__doc__)
+
 fileB = args["<fB>"]
 fileS = args["<fS>"]
 fileW = args["<fW>"]
@@ -46,18 +50,19 @@ x1 = (L - d) / 2
 x2 = (L + d) / 2
 dx = L / len(B)
 
-plt.fill_between(axis, 0, By, facecolor="r")
+plt.fill_between(axis, 0, By, facecolor="red")
 plt.fill_between(axis, By, By + Sy, facecolor="lime")
-plt.fill_between(axis, By + Sy, By + Sy + Wy, facecolor="b")
+plt.fill_between(axis, By + Sy, By + Sy + Wy, facecolor="blue")
 plt.fill_between(axis, By + Sy + Wy, By + Sy + Wy + Ey, facecolor="gray")
 plt.xlim([dx, L - dx])
 plt.ylim([0, 1])
 plt.axvline(x=x1, color="black")
 plt.axvline(x=x2, color="black")
+plt.xlabel("Box coordinate (nm)")
 
 ext = "pdf" if args["--pdf"] else "png"
-plotname = "phase_plot." + ext
-plt.savefig(plotname)
-print("Plot saved in", plotname)
+plotname = args["--out"] + "." + ext
+plt.savefig(plotname, bbox_inches='tight')
+print("Phase plot saved in", plotname)
 
 
