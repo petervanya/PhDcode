0fc5c9a3f340576a6cc7fc4fafaf782512fbf04a
petervanya <peter.vanya@gmail.com>
2017-08-22 11:11:13 +0100

    Added Yamamoto coarse-graining

--- PTFE_code/lmbda2wt.py
+++ PTFE_code/lmbda2wt.py
@@ -4,7 +4,6 @@ Convert water uptake lmbda to water content as a percentage.
 
 03/05/17
 """
-import matplotlib.pyplot as plt
 
 
 def lmbda2wt(l, N, Nmc, Nbm):
@@ -15,12 +14,25 @@ def lmbda2wt(l, N, Nmc, Nbm):
 
 Nbm = 5
 Nmc = 15
-N = 192000
-lmbdas = list(range(4, 25))
+rho = 3
+L = 40.0
+N = rho * L**3
+lmbdas = list(range(4, 25, 2))
 wts = [lmbda2wt(l, N, Nmc, Nbm) for l in lmbdas]
 
+#import matplotlib.pyplot as plt
 #plt.plot(lmbdas, wts, "*-")
 #plt.show()
 
+print("Wu parametrisation:")
+print("Beads per monomer: %i | Monomers per chain: %i" % (Nbm, Nmc))
 for i in range(len(lmbdas)):
-    print("%i   %.1f" % (lmbdas[i], 100*wts[i]))
+    print("%2.i   %.3f" % (lmbdas[i], wts[i]))
+
+print("Yamamoto parametrisation:")
+Nbm = 6
+lmbdas = list(range(2, 25, 2))
+wts_y = [l / (l + 4 * Nbm) for l in lmbdas]
+print("Beads per monomer: %i | Monomers per chain: %i" % (Nbm, Nmc))
+for i in range(len(lmbdas)):
+    print("%2.i   %.3f" % (lmbdas[i], wts_y[i]))
