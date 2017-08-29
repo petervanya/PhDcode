19d6aa50f5fa59baf33df2dfcded411754a62139
petervanya <peter.vanya@gmail.com>
2017-08-29 14:53:10 +0100

    New option

--- Lib/struct_fact.py
+++ Lib/struct_fact.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 """
 Usage:
-    struct_fact.py <rdf> [--rho <rho> --L <L> --plot --f <f>]
-    struct_fact.py <rdf> si [--rho <rho> --L <L> --rc <rc> --nm <nm> --plot --f <f>]
+    struct_fact.py <rdf> [--rho <rho> --L <L> --plot --f <f> --limit-k <kl>]
+    struct_fact.py <rdf> si [--rho <rho> --L <L> --rc <rc> --nm <nm>]
+                            [--plot --f <f> --limit-k <kl>]
 
 Compute the structure factor from the RDF.
 
 Options:
-    --L <L>        Box size [default: 10]
-    --rho <rho>    DPD density [default: 3.0]
-    --rc <rc>      DPD cutoff [default: 0.814]
-    --nm <nm>      CG degree [default: 6]
-    --f <f>        Sampling freq, multiple of N-points to use for k's [default: 1]
+    --L <L>          Box size [default: 10]
+    --rho <rho>      DPD density [default: 3.0]
+    --rc <rc>        DPD cutoff [default: 0.814]
+    --nm <nm>        CG degree [default: 6]
+    --f <f>          Sampling freq, multiple of N-points to use for k's [default: 1]
+    --limit-k <kl>   Multiply max k = 2pi/dr by a factor between 0 and 1 [default: 1]
 
 pv278@cam.ac.uk, 21/08/17
 """
@@ -23,8 +25,6 @@ from docopt import docopt
 
 def gen_sk(r, gr, k, rho=3.0):
     dr = r[1] - r[0]
-    print(rho)
-    print(dr)
     N = len(r)
     Sk = [1 + 4 * pi * rho * sum([r[j]**2 * (gr[j] - 1.0) * \
             sin(ki * r[j]) / (ki * r[j]) * dr for j in range(N)]) for ki in k]
@@ -40,6 +40,7 @@ if __name__ == "__main__":
     rho = float(args["--rho"])
     L = float(args["--L"])
     f = int(args["--f"])
+    kl = float(args["--limit-k"])
     r, gr = A[:, 0], A[:, 1]
 
     print("===== Structure factor =====")
@@ -55,9 +56,10 @@ if __name__ == "__main__":
     N = len(r)
     dr = r[1] - r[0]
     kmin = 2 * pi / max(r)
-    kmax = 2 * pi / dr
-#    k = np.linspace(0, kmax, f*N+1)[1:]
-    k = np.linspace(0, max(r), f*N+1)[1:]
+    kmax = 2 * pi / dr * kl
+#    kmax = max(r)
+    print("Maximum k: %.3f" % kmax)
+    k = np.linspace(0, kmax, f*N+1)[1:]
     Sk = gen_sk(r, gr, k, rho)
     outname = "sk_si.out" if args["si"] else "sk.out"
     np.savetxt(outname, np.c_[k, Sk])
